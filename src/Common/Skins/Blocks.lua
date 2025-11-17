1724a6b3655a1acfe6a531c5b40f9554289e097b
Dylan <57853417+4812571@users.noreply.github.com>
2025-11-17 01:21:28 -0600

    feat: various skins tab

---
+++ src/Common/Skins/Blocks.lua
@@ -0,0 +1,113 @@
+local Blocks = {
+    -- Terrain
+    Bedrock = "rbxassetid://140218636439061",
+    Obsidian = "rbxassetid://86245010705496",
+    CryingObsidian = "rbxassetid://117288180306709",
+    Stone = "rbxassetid://123378639695928",
+    Dirt = "rbxassetid://92704279856523",
+    Sand = "rbxassetid://80488232146971",
+    Water = "rbxassetid://125016972951345",
+    Grass = "rbxassetid://81274839799960",
+    Snow = "rbxassetid://110023016916083",
+
+    -- Alien
+    EvilRock = "rbxassetid://122385783037495",
+    AlienBlue = "rbxassetid://88555009036632",
+    AlienRed = "rbxassetid://104741192216301",
+    AlienTurqoise = "rbxassetid://77368915527604",
+    AlienPurple = "rbxassetid://85498044612185",
+    AlienMagenta = "rbxassetid://121249127507201",
+    Magic = "rbxassetid://114952573510615",
+
+    -- Hot Terrain
+    Magma = "rbxassetid://134362400267283",
+    DarkMagma = "rbxassetid://100818312309405",
+    Lava = "rbxassetid://87718740521862",
+    Volcano = "rbxassetid://91746970219917",
+    Portal = "rbxassetid://90624934178722",
+
+    -- Building
+    Cobblestone = "rbxassetid://122966790860765",
+    MossyCobblestone = "rbxassetid://138488789264345",
+    BrownBricks = "rbxassetid://132191289070179",
+    BlueBricks = "rbxassetid://129021758166791",
+    RedBricks = "rbxassetid://101322357019798",
+    Planks = "rbxassetid://118607951012531",
+    Log = "rbxassetid://118167336590132",
+    Target = "rbxassetid://126995910697085",
+    Bookshelf = "rbxassetid://138216572173171",
+
+    -- Factory
+    Conveyor = "rbxassetid://120212147383896",
+    Hazard = "rbxassetid://78136619529991",
+    Tile = "rbxassetid://128591832209095",
+    Lantern = "rbxassetid://83901016332619",
+    Biohazard = "rbxassetid://78814667841494",
+
+    -- Valuables
+    CoalOre = "rbxassetid://101740322947594",
+    Coal = "rbxassetid://98872245838411",
+    IronOre = "rbxassetid://76091000696580",
+    Iron = "rbxassetid://84658004668798",
+    LapisOre = "rbxassetid://131002034520763",
+    Lapis = "rbxassetid://137731236335453",
+    RedstoneOre = "rbxassetid://91802964535929",
+    GoldOre = "rbxassetid://71630049894687",
+    Gold = "rbxassetid://83788568441368",
+    EmeraldOre = "rbxassetid://136097519937877",
+    Emerald = "rbxassetid://131739356354454",
+    DiamondOre = "rbxassetid://83519746509201",
+    Diamond = "rbxassetid://109807619298074",
+
+    -- Natural
+    Pumpkin = "rbxassetid://86337828251659",
+    JackOLantern = "rbxassetid://71565290963168",
+    Haybale = "rbxassetid://97746239712043",
+    Honeycomb = "rbxassetid://108038812131691",
+    Leaves = "rbxassetid://139873996799981",
+    Lemon = "rbxassetid://139160631300428",
+    Strawberry = "rbxassetid://118726034568108",
+    Carrot = "rbxassetid://85127399187285",
+    Cucumber = "rbxassetid://127242528747644",
+    Mushroom = "rbxassetid://95646820642033",
+    PricklyCactus = "rbxassetid://121106532773707",
+    PointyCactus = "rbxassetid://85870552825554",
+
+    -- Seasonal
+    Squash = "rbxassetid://102469525877276",
+    SinisterPumpkin = "rbxassetid://134235938284358",
+    SinisterJackOLantern = "rbxassetid://113814251890916",
+    AutumnLeaves = "rbxassetid://120413618228842",
+    Bacon = "rbxassetid://91392911582459",
+    Turkey = "rbxassetid://70682743737785",
+
+    -- Snacks & Candy
+    Cookie = "rbxassetid://139741518582345",
+    ChocolateCookie = "rbxassetid://90630656915717",
+    Chocolate = "rbxassetid://133409015492150",
+    ChocolateHearts = "rbxassetid://123407886794068",
+    FancyChocolate = "rbxassetid://118681663665356",
+    PinkBubblegum = "rbxassetid://133965960820918",
+    BlueBubblegum = "rbxassetid://140349872907595",
+    MintChocolateIcecream = "rbxassetid://119010402353961",
+    Sprinkles = "rbxassetid://119798501634866",
+    Gumdrops = "rbxassetid://73268076120781",
+    Wafers = "rbxassetid://139171042415201",
+    CottonCandy = "rbxassetid://123073139472809",
+
+    -- Fabric
+    Patchwork = "rbxassetid://80005463774010",
+    Quilt = "rbxassetid://95363162079394",
+    Flowers = "rbxassetid://105526013046164",
+    Plaid = "rbxassetid://124817853581093",
+
+    -- Camo
+    LeopardCamo = "rbxassetid://87283925938716",
+    CheetahCamo = "rbxassetid://121762608274745",
+    TigerCamo = "rbxassetid://93631603528484",
+    GreenCamo = "rbxassetid://137054198559256",
+    BlueCamo = "rbxassetid://85623336909351",
+    DesertCamo = "rbxassetid://75202847117882",
+}
+
+return Blocks
